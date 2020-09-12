06e8d67897dfd946d76a0493a4102b9e036eccf0
Jay Barria <jaybarria@gmail.com>
2020-09-12 10:09:34 +0800

    Update ImageUtilities

--- imageutilities/src/main/java/com/github/jairrab/imageutilities/ImageUtilities.kt
+++ imageutilities/src/main/java/com/github/jairrab/imageutilities/ImageUtilities.kt
@@ -2,17 +2,17 @@ package com.github.jairrab.imageutilities
 
 import android.content.Context
 import android.net.Uri
-import com.github.jairrab.imageutilities.lib.ImageUtilityLibrary
+import com.github.jairrab.imageutilities.lib.ImageUtilitiesLibrary
 import com.github.jairrab.safutilities.SafUtilities
 import java.io.File
 
-interface ImageUtility {
+interface ImageUtilities {
     fun getResizedImage(sourceUri: Uri, outputFile: File, width: Int, quality: Int): File
 
     companion object {
-        fun getInstance(context: Context): ImageUtility {
+        fun getInstance(context: Context): ImageUtilities {
             val safUtilities = SafUtilities.getInstance(context)
-            return ImageUtilityLibrary.getInstance(context, safUtilities)
+            return ImageUtilitiesLibrary.getInstance(context, safUtilities)
         }
     }
 }
--- imageutilities/src/main/java/com/github/jairrab/imageutilities/lib/ImageUtilitiesLibrary.kt
+++ imageutilities/src/main/java/com/github/jairrab/imageutilities/lib/ImageUtilitiesLibrary.kt
@@ -2,26 +2,26 @@ package com.github.jairrab.imageutilities.lib
 
 import android.content.Context
 import android.net.Uri
-import com.github.jairrab.imageutilities.ImageUtility
+import com.github.jairrab.imageutilities.ImageUtilities
 import com.github.jairrab.imageutilities.lib.helpers.*
 import com.github.jairrab.safutilities.SafUtilities
 import java.io.File
 
-internal class ImageUtilityLibrary private constructor(
+internal class ImageUtilitiesLibrary private constructor(
     private val imageResizer: ImageResizer,
-) : ImageUtility {
+) : ImageUtilities {
     override fun getResizedImage(sourceUri: Uri, outputFile: File, width: Int, quality: Int): File {
         return imageResizer.execute(sourceUri, width, outputFile, quality)
     }
 
     companion object {
-        fun getInstance(context: Context, safUtilities: SafUtilities): ImageUtilityLibrary {
+        fun getInstance(context: Context, safUtilities: SafUtilities): ImageUtilitiesLibrary {
             val imageResizer = ImageResizer(
                 jpegUtility = JpegUtility(),
                 imageDimension = ImageDimension(safUtilities),
                 bitmapUtilities = BitmapUtilities(BitmapRotation(context), safUtilities)
             )
-            return ImageUtilityLibrary(imageResizer)
+            return ImageUtilitiesLibrary(imageResizer)
         }
     }
 }
